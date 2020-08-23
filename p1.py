0d18861208314168b0b308760b2869804d2bc8a2
Rogerio Miguel Coelho <rogerio.coelho@petrobras.com.br>
2020-08-23 10:13:49 -0300

    primeiroArquivo

---
+++ p1.py
@@ -0,0 +1,14 @@
+'''
+Autor: Rogerio Coelho
+E-mail: rogerio.coelho@faesa.br
+Repositorio: https://github.com/rogeriomcoelho/python.git
+Descricao: Primeiro exemplo de Python. Imprimir
+           uma mensagem no terminal.
+Comentario de bloco sao iniciado com 3 aspas seguidas.
+Cometario de uma únioca linha #
+'''
+
+texto = input("Digite seu nome = ")
+
+#Cometario de uma unica linha utilize jogo da velha.
+print("Seu nome = " + texto)
